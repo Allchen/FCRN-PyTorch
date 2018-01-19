0cbebc7ce44f8b5ac436bff19f3c6fa5a8552546
zergzzlun <zergzzlun@gmail.com>
2018-01-19 17:45:24 +0800

    Add paper address to README; Modified nyud_dataset.py about data address.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FCRN-PyTorch
 PyTorch implementation of Fully Convolutional Residual Networks(FCRN).
+The paper can be found [here](https://arxiv.org/pdf/1606.00373.pdf)
--- nyud_dataset.py
+++ nyud_dataset.py
@@ -6,7 +6,10 @@ import torchvision.transforms as transforms
 from folder_s import NYUDFolder
 
 
-def get_nyud_train_set(image_size=(480, 640), batch_size=16):
+def get_nyud_train_set(image_size=(480, 640), batch_size=16,
+                       root_rgb='data/nyud/train/RGB/',
+                       root_depth='data/nyud/train/depth_real/'
+                       ):
 
     transform = transforms.Compose([
         transforms.Scale(image_size),
@@ -14,8 +17,8 @@ def get_nyud_train_set(image_size=(480, 640), batch_size=16):
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
     train_set = NYUDFolder(
-        root='data/nyud/train/RGB/',
-        root_depth='data/nyud/train/depth_real/',
+        root=root_rgb,
+        root_depth=root_depth,
         transform=transform,
         )
     train_set_loader = torch.utils.data.DataLoader(
@@ -27,7 +30,10 @@ def get_nyud_train_set(image_size=(480, 640), batch_size=16):
     return train_set_loader
 
 
-def get_nyud_test_set(image_size=(256, 256)):
+def get_nyud_test_set(image_size=(256, 256),
+                      root_rgb='data/nyud/test/RGB/',
+                      root_depth='data/nyud/test/depth_real/'
+                      ):
 
     if image_size is not None:
         if not isinstance(image_size, tuple):
@@ -40,8 +46,8 @@ def get_nyud_test_set(image_size=(256, 256)):
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
     test_set = NYUDFolder(
-        root='data/nyud/test/RGB/',
-        root_depth='data/nyud/test/depth_real/',
+        root=root_rgb,
+        root_depth=root_depth,
         transform=transformer
         )
     test_set_loader = torch.utils.data.DataLoader(
